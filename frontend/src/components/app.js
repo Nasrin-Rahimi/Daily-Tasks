56d5b9f58e3bd4d7015eacf4c74ff1546a065055
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-02-20 08:04:13 -0500

    change app to get user istead of tasks at first

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,20 @@
 class App{
     constructor(){
-        this.tasks = new Tasks()
+         // this.tasks = new Tasks();
+       this.user = {};
+       this.adapter = new UsersAdapter();
+       this.initBindingsAndEventListener();
+    }
+
+    initBindingsAndEventListener(){
+        this.userForm = document.getElementById('new-user-form');
+        this.userForm.addEventListener('submit', this.findOrCreateUser.bind(this));
+    }
+
+    findOrCreateUser(e){
+        e.preventDefault();
+        const userName = e.target.children[0].value;
+        this.adapter.findOrCreateUser(userName).then(user => this.user = new User(user));
+        console.log(this)
     }
 }
