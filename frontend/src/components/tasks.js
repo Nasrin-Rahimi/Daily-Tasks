804fb7b0365effab70abe8cb4e3cfe310ea6322a
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-02-22 15:42:15 -0500

    add eventlistener for new task button

--- frontend/src/components/tasks.js
+++ frontend/src/components/tasks.js
@@ -10,8 +10,15 @@ class Tasks{
     initBindingsAndEventListener(){
         this.tasksContainer = document.getElementById('tasks_container');
         this.messageDiv = document.getElementById('message-div');
-        this.newToyBtn = document.getElementById('new-toy-btn');
+        this.newTaskBtn = document.getElementById('new-task-btn');
+        this.newTaskContainer = document.getElementById('new-task-container');
         this.tasksContainer.addEventListener('change', this.doneTask.bind(this));
+        this.newTaskBtn.addEventListener('click', this.newTask.bind(this));
+
+    }
+
+    loadTasks(tasksJSON){
+        tasksJSON.forEach(task => this.tasks.push(new Task(task)));
     }
 
     doneTask(e){
@@ -20,14 +27,15 @@ class Tasks{
         this.adapter.updateTask(task);
         e.target.parentNode.classList.add('checked');
     }
-
-    loadTasks(tasksJSON){
-         tasksJSON.forEach(task => this.tasks.push(new Task(task)));
-        //  console.log(this.tasks);
+    
+    newTask(e){
+        this.newTaskContainer.style.display == 'block' ? 
+            this.newTaskContainer.style.display = 'none' : 
+            this.newTaskContainer.style.display = 'block';
     }
 
     renderTasks(){
-        this.newToyBtn.style.display = 'block';
+        this.newTaskBtn.style.display = 'block';
         if (this.tasks.length > 0){
             this.tasksContainer.style.display = "block";
             this.messageDiv.style.display = "none";
