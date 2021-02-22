01be25b7448d083da9913f03886fec1bdfeb2680
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-02-22 16:08:48 -0500

    add welcome to user and do styling

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,13 +7,19 @@ class App{
     }
 
     initBindingsAndEventListener(){
-        this.userForm = document.getElementById('new-user-form');
+        this.userContainer = document.getElementById('user-container');
+        this.welcomeDiv = document.getElementById('welcome');
+        this.userForm = document.getElementById('user-form');
         this.userForm.addEventListener('submit', this.findOrCreateUser.bind(this));
     }
 
     findOrCreateUser(e){
         e.preventDefault();
+        this.userContainer.style.display = 'none';
         const userName = e.target.children[0].value;
-        this.adapter.findOrCreateUser(userName).then(user => new User(user));
+        this.adapter.findOrCreateUser(userName)
+        .then(user => new User(user))
+        .then(user => this.welcomeDiv.innerHTML = `<h2>Welcome ${user.userName}</h2>`);
+        
     }
 }
