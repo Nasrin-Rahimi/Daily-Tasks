455c9c368b894483879156730a30e7a94736f54b
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-02-25 07:06:39 -0500

    add alt attribute for img tag

--- frontend/src/components/task.js
+++ frontend/src/components/task.js
@@ -11,13 +11,13 @@ class Task{
         if (this.done == true){
             return `<li class="task checked"><input type="checkbox" id="${this.id}" checked />
             <label for="${this.id}">${this.title}</label>
-            <img class="taskImage" src="${this.imageUrl}"/>
+            <img class="taskImage" src="${this.imageUrl}" alt="Task Image"/>
             <button id="${this.id}" class="delete" >Delete Task</button>
             </li>`
         } else {
             return `<li class="task"><input type="checkbox" id="${this.id}" />
             <label for="${this.id}">${this.title}</label>
-            <img class="taskImage" src="${this.imageUrl}"/>
+            <img class="taskImage" src="${this.imageUrl}" alt="Task Image"/>
             <button id="${this.id}" class="delete" >Delete Task</button>
             </li>`
         }
